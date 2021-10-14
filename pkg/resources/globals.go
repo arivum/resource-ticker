5cdf8cbe8e08a4121cbe4b027b833faa70dbf4d1
Sascha Winkelhofer <sascha.winkelhofer@arivum.de>
2021-10-15 01:38:00 +0200

    added license

--- pkg/resources/globals.go
+++ pkg/resources/globals.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021, arivum.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 package resources
 
 import "errors"
